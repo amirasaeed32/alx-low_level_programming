02081731977556d209f4541bdae00fe56e6e77dc
amirasaeed32 <amirasaeed637@gmail.com>
2023-10-26 09:28:58 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,15 +6,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
+	unsigned int n = 0, m = 1;
+	int l;
 
-	if (!b)
+	if (b == '\0')
 		return (0);
-	while (*b)
+	for (l = 0; b[l];)
+		l++;
+	for (l -= 1; l >= 0; l--)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[l] != '0' && b[l] != '1')
 			return (0);
-		n = n * 2 + (*b++ - '0');
+		n += (b[l] - '0') * m;
+		m *= 2;
 	}
 	return (n);
 }
